bdc56b25dc56a05e4b6b1f0241bbc91d6f002802
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-01-05 21:51:58 +0530

    Default code

---
+++ LoggingWebDemo/src/main/java/com/rit/boot/LoggingWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rit.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingWebDemoApplication.class, args);
+	}
+
+}
