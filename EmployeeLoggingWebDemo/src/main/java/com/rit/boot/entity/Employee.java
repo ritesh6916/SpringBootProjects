f017df58fc2e63be9e848af57c564ca69cc20a31
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-01-06 12:24:08 +0530

    entity created with basic fields.

--- EmployeeLoggingWebDemo/src/main/java/com/rit/boot/entity/Employee.java
+++ EmployeeLoggingWebDemo/src/main/java/com/rit/boot/entity/Employee.java
@@ -1,5 +1,80 @@
 package com.rit.boot.entity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Employee {
 
+	private int id;
+	private String name;
+	private String email;
+	private String phone;
+	private String company;
+	private String role;
+	private String department;
+	
+	@Autowired
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(int id, String name, String email, String phone, String company, String role, String department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.company = company;
+		this.role = role;
+		this.department = department;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", company="
+				+ company + ", role=" + role + ", department=" + department + "]";
+	}
+	
 }
