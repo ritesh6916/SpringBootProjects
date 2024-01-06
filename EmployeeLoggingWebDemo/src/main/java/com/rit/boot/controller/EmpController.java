e9ab7e899422c86fb684e06cc86dfbef2ca5d8da
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-01-06 12:07:50 +0530

    home Controller added

--- EmployeeLoggingWebDemo/src/main/java/com/rit/boot/controller/EmpController.java
+++ EmployeeLoggingWebDemo/src/main/java/com/rit/boot/controller/EmpController.java
@@ -1,5 +1,15 @@
 package com.rit.boot.controller;
 
-public class EmpController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class EmpController {
+	
+	@RequestMapping(path = "/home", method = RequestMethod.GET)
+	public String HomeController() {
+		
+		return "home";
+	}
 }
