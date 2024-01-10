9e67f0bb915fb57161da738f5e31509a49a44312
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-01-10 18:46:21 +0530

    PUT request added for update.

--- EmployeeWeb/src/main/java/com/rit/boot/controller/EmpController.java
+++ EmployeeWeb/src/main/java/com/rit/boot/controller/EmpController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +53,10 @@ public class EmpController {
 	}
 
 	@PostMapping(path = "/employee")
-	public void addEmployee(@ModelAttribute Employee employee) {
+	public void addEmployee(@ModelAttribute Employee employee)
+	// public void addEmployee(@RequestBody Employee employee) -> We can also user
+	// @RequestBody to accept data
+	{
 		logger.debug("request to add employee with ID: " + employee.getId() + " name: " + employee.getName());
 		empData.addEmployee(employee);
 
@@ -63,7 +68,16 @@ public class EmpController {
 	public void deleteEmployee(@PathVariable("id") int id) {
 		logger.debug("Delete request for ID: " + id);
 		empData.deleteEmployee(id);
-		
+
 		// Delete request form POSTMAN ex: "http://localhost:8080/MyApp/employee/6916"
+	}
+
+	@PutMapping("/employee")
+	public void updateEmployeeBiId(@RequestBody Employee employee) {
+		if (employee != null) {
+			empData.updateEmployee(employee);
+		}
+
+		// we can make a PUT request giving the details in Postman-> Body RAW (JSON)
 	}
 }
