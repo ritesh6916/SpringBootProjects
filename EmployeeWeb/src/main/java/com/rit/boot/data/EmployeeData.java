25c41bb4de9c3310116d190aaaa463e4f143adeb
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-01-10 18:45:48 +0530

    updateEmployee added

--- EmployeeWeb/src/main/java/com/rit/boot/data/EmployeeData.java
+++ EmployeeWeb/src/main/java/com/rit/boot/data/EmployeeData.java
@@ -12,52 +12,57 @@ import com.rit.boot.entity.Employee;
 public class EmployeeData {
 
 	private static ArrayList<Employee> empList = new ArrayList<>();
-	
+
 	Logger logger = LoggerFactory.getLogger(EmployeeData.class);
-	
+
 	static {
-		
-		empList.add(new Employee(6916,"Ritesh","riteshsingh0500@gmail.com","8002208399","Cognizant","Java Dev","IT"));
-		empList.add(new Employee(1111,"Shyam","shaym@gmail.com","8002208399","Calsof","Security","IT"));
-		empList.add(new Employee(2222,"Mohan","mohan@gmail.com","8937483739","Wipro","Support","IT"));
-		empList.add(new Employee(3333,"Sohan","sohan@gmail.com","3339383943","nowhere","Java Dev","IT"));
+
+		empList.add(
+				new Employee(6916, "Ritesh", "riteshsingh0500@gmail.com", "8002208399", "Cognizant", "Java Dev", "IT"));
+		empList.add(new Employee(1111, "Shyam", "shaym@gmail.com", "8002208399", "Calsof", "Security", "IT"));
+		empList.add(new Employee(2222, "Mohan", "mohan@gmail.com", "8937483739", "Wipro", "Support", "IT"));
+		empList.add(new Employee(3333, "Sohan", "sohan@gmail.com", "3339383943", "nowhere", "Java Dev", "IT"));
 	}
-	
+
 	public void addEmployee(Employee e) {
 		empList.add(e);
 	}
-	
-	public ArrayList<Employee> getAllEmployee(){
-		
+
+	public ArrayList<Employee> getAllEmployee() {
+
 		return empList;
 	}
-	
-	public Employee getEmployeeById(int id){
-		
-		for(Employee e: empList) {
-			if(e.getId()==id) {
+
+	public Employee getEmployeeById(int id) {
+
+		for (Employee e : empList) {
+			if (e.getId() == id) {
 				return e;
 			}
 		}
 		return null;
 	}
-	
+
 	public void deleteEmployee(int id) {
-		int indexOf=0;
-		for(Employee e: empList) {
-			if(e.getId()==id) {
+		int indexOf = 0;
+		for (Employee e : empList) {
+			if (e.getId() == id) {
 				indexOf = empList.indexOf(e);
 			}
 		}
-		if(indexOf!=0) {
+		if (indexOf != 0) {
 			empList.remove(indexOf);
-		}
-		else {
+		} else {
 			try {
-			throw new NullPointerException();
-			}catch(NullPointerException e) {
-				logger.debug("The data is not present for ID: "+id);
+				throw new NullPointerException();
+			} catch (NullPointerException e) {
+				logger.debug("The data is not present for ID: " + id);
 			}
 		}
+	}
+
+	public void updateEmployee(Employee emp) {
+		deleteEmployee(emp.getId());
+		addEmployee(emp);
 	}
 }
