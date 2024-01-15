8182fbe7682b8b6f6389b6680fbcb8e2d7d941b2
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-01-15 16:19:35 +0530

    custom health info coded.

--- EmployeeWeb/src/main/java/com/rit/boot/monitor/CustomHealthIndicator.java
+++ EmployeeWeb/src/main/java/com/rit/boot/monitor/CustomHealthIndicator.java
@@ -2,13 +2,19 @@ package com.rit.boot.monitor;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
 
-public class CustomHealthIndicator implements HealthIndicator{
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
 
 	@Override
 	public Health health() {
-		
-		return null;
-	}
+		boolean error = false; // we can change the flag based on our requirement
+		if (error) {
+			return Health.down().withDetail("Cause", 404).build();
+		}
+		return Health.up().withDetail("Sab thik h:", "Radhe-Radhe").build();
 
+		// http://localhost:8080/MyApp/actuator/health (to invoke)
+	}
 }
