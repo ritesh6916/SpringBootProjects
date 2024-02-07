07368e97e1daefabbf4848bbee2eaccc87dd2a19
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-02-07 23:43:55 +0530

    default code

---
+++ BookRestCrudDB/src/main/java/com/ritesh/boot/BookRestCrudDbApplication.java
@@ -0,0 +1,13 @@
+package com.ritesh.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRestCrudDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRestCrudDbApplication.class, args);
+	}
+
+}
