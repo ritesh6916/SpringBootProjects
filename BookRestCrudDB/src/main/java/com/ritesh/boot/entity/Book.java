b23027354a63fd28d45e046231b502ecf426ea84
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-02-08 00:16:16 +0530

    basic code done.

---
+++ BookRestCrudDB/src/main/java/com/ritesh/boot/entity/Book.java
@@ -0,0 +1,71 @@
+package com.ritesh.boot.entity;
+
+public class Book {
+
+	private int id;
+	private String name;
+	private String publication;
+	private String author;
+	private double price;
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(int id, String name, String publication, String author, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.publication = publication;
+		this.author = author;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPublication() {
+		return publication;
+	}
+
+	public void setPublication(String publication) {
+		this.publication = publication;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", publication=" + publication + ", author=" + author + ", price="
+				+ price + "]";
+	}
+
+}
