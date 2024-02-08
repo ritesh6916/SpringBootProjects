d22c8798fd6b0aa695df1aa56b4b1a11c875786f
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-02-08 23:46:29 +0530

    Tested DB connection by inserting one object.

--- BookRestCrudDB/src/main/java/com/ritesh/boot/BookRestCrudDbApplication.java
+++ BookRestCrudDB/src/main/java/com/ritesh/boot/BookRestCrudDbApplication.java
@@ -2,12 +2,25 @@ package com.ritesh.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.ritesh.boot.persistence.BookRepository;
 
 @SpringBootApplication
 public class BookRestCrudDbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookRestCrudDbApplication.class, args);
+		ApplicationContext context = SpringApplication.run(BookRestCrudDbApplication.class, args);
+		BookRepository bookRepository = context.getBean(BookRepository.class);
+
+		// The code is written to test DB connection
+		/*
+		 * Book book = new Book(); book.setName("The Java");
+		 * book.setAuthor("Ritesh Singh"); book.setPublication("Basistha publication");
+		 * book.setPrice(255.50);
+		 * 
+		 * bookRepository.save(book);
+		 */
 	}
 
 }
