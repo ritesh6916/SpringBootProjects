438fb22fa77be7d8a615309447438609328341bc
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-02-08 23:43:55 +0530

    added @Repository

--- BookRestCrudDB/src/main/java/com/ritesh/boot/persistence/BookRepository.java
+++ BookRestCrudDB/src/main/java/com/ritesh/boot/persistence/BookRepository.java
@@ -1,9 +1,11 @@
 package com.ritesh.boot.persistence;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ritesh.boot.entity.Book;
 
-public interface BookRepository extends CrudRepository<Book,Integer> {
+@Repository
+public interface BookRepository extends CrudRepository<Book, Integer> {
 
 }
