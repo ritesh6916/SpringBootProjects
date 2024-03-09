83dc25b2b5982429a0eb721490ed619705be01fe
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-09 16:08:23 +0530

    default test

---
+++ BootSurveyApi/src/test/java/com/ritesh/survey/BootSurveyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritesh.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSurveyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
