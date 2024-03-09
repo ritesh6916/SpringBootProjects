98519ba5f0e448193c3aca689a963a0141dfecee
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-09 16:09:44 +0530

    default main-app.

---
+++ BootSurveyApi/src/main/java/com/ritesh/survey/BootSurveyApiApplication.java
@@ -0,0 +1,13 @@
+package com.ritesh.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSurveyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSurveyApiApplication.class, args);
+	}
+
+}
