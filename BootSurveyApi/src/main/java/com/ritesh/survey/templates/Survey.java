96212fc23d6d7800ae756ebab4e5e2a84e99fc96
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-09 20:37:31 +0530

    Survey template coded.

---
+++ BootSurveyApi/src/main/java/com/ritesh/survey/templates/Survey.java
@@ -0,0 +1,59 @@
+package com.ritesh.survey.templates;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Survey {
+
+	private String id;
+	private String title;
+	private String description;
+	private List<Question> questions;
+
+	public Survey(String id, String title, String description, List<Question> questions) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.questions = questions;
+	}
+
+	public Survey() {
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+
+}
