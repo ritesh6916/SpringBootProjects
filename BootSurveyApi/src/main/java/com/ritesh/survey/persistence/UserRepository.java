4e4bbba06aee9bce78322e2dd5389dd9f11032dc
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-10 17:29:10 +0530

    Custom method added: findByRole

--- BootSurveyApi/src/main/java/com/ritesh/survey/persistence/UserRepository.java
+++ BootSurveyApi/src/main/java/com/ritesh/survey/persistence/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ritesh.survey.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.ritesh.survey.templates.UserDetails;
 @Repository
 public interface UserRepository extends JpaRepository<UserDetails, Long> {
 
+	List<UserDetails> findByRole(String role);
 }
