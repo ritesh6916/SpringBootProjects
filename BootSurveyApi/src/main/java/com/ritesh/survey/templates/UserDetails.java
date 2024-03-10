26b8a763159de08c04c60ece030d5f0e38e47e67
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-10 15:10:27 +0530

    Entity created for UserDetails.

---
+++ BootSurveyApi/src/main/java/com/ritesh/survey/templates/UserDetails.java
@@ -0,0 +1,50 @@
+package com.ritesh.survey.templates;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserDetails {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private String name;
+	private String role;
+
+	public UserDetails(String name, String role) {
+		super();
+		this.name = name;
+		this.role = role;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDetails [id=" + id + ", name=" + name + ", role=" + role + "]";
+	}
+}
