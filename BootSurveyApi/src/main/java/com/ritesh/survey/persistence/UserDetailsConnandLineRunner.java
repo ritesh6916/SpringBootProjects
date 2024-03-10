d81a562f1782a14599a357c1bc3e33ebeb600175
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-10 16:24:45 +0530

    added new users at server start.

--- BootSurveyApi/src/main/java/com/ritesh/survey/persistence/UserDetailsConnandLineRunner.java
+++ BootSurveyApi/src/main/java/com/ritesh/survey/persistence/UserDetailsConnandLineRunner.java
@@ -1,5 +1,34 @@
 package com.ritesh.survey.persistence;
 
-public class UserDetailsConnandLineRunner {
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.ritesh.survey.templates.UserDetails;
+
+@Component
+public class UserDetailsConnandLineRunner implements CommandLineRunner {
+
+	private UserRepository userRepository;
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	public UserDetailsConnandLineRunner(UserRepository repository) {
+		this.userRepository = repository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new UserDetails("Ritesh", "ADMIN"));
+		userRepository.save(new UserDetails("Dummy", "ADMIN"));
+		
+		List<UserDetails> users = userRepository.findAll();
+		for (UserDetails user : users) {
+			logger.info(user.toString());
+		}
+	}
 
 }
